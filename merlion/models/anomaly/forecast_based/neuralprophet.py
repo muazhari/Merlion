2a156c0606123202d48f5103c698651a27e619ab
root <kharisma.azhari02@gmail.com>
2023-01-02 11:54:53 +0000

    Add forecast based anomaly for NeuralProphet model

---
+++ merlion/models/anomaly/forecast_based/neuralprophet.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2023 salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+#
+"""
+Adaptation of NeuralNeuralProphet forecasting model to anomaly detection.
+"""
+
+from merlion.models.anomaly.forecast_based.base import ForecastingDetectorBase
+from merlion.models.anomaly.base import DetectorConfig
+from merlion.models.forecast.NeuralProphet import NeuralProphetConfig, NeuralProphet
+from merlion.post_process.threshold import AggregateAlarms
+
+
+class NeuralProphetDetectorConfig(NeuralProphetConfig, DetectorConfig):
+    _default_threshold = AggregateAlarms(alm_threshold=3)
+
+
+class NeuralProphetDetector(ForecastingDetectorBase, NeuralProphet):
+    config_class = NeuralProphetDetectorConfig
